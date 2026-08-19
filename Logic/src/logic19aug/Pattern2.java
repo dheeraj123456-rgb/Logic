c253abeb999826cf14d64b32b7bee6478a55ab01
dheer <dheer@Dheeraj>
2026-08-19 16:35:04 +0530

    test

--- Logic/src/logic19aug/Pattern2.java
+++ Logic/src/logic19aug/Pattern2.java
@@ -1,5 +1,19 @@
 package logic19aug;
 
 public class Pattern2 {
+ 
+	public static void main(String[] args) {
+		pattern3(5);
+	}
 
+
+static void pattern3(int n) {
+	for(int row = 1; row <= n;row++) {
+		for(int col = 1; col <= n-row+1; col++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+	
+  }
 }
